f0ea425128a4ab27ad9dccf537b584cf8785ade9
Paulo Amaral <pauloamaral2006@yahoo.com.br>
2026-03-11 10:36:54 -0300

    Aula 01

---
+++ type-number.js
@@ -0,0 +1,33 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+let operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica);
+
+operacaoMatematica = primeiroNumero * segundoNumero;
+
+console.log(operacaoMatematica);
+
+operacaoMatematica = primeiroNumero / segundoNumero;
+
+console.log(operacaoMatematica);
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = 0.5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao);
+
+// NA => Not a number (não é um número)
+
+const alura = "Alura";
+
+console.log(alura * primeiroNumero);
---
+++ type-string.js
@@ -0,0 +1,14 @@
+const texto1 = "Olá mundo!";
+const texto2 = "Olá mundo!";
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+let citacao = 'O Leo disse "oi"';
+console.log(citacao);
+citacao = "O Leo disse 'oi'";
+console.log(citacao);
+
+// concatenação (+)
+citacao = "Meu nome é ";
+const meuNome = "Paulo";
+console.log(citacao + meuNome);
+//template string ou template literal
