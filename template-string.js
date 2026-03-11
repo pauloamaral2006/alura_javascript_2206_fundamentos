d54fac41f1c29346af1c83f3e66df531ef85c12b
Paulo Amaral <pauloamaral2006@yahoo.com.br>
2026-03-11 13:49:22 -0300

    Aula 04

---
+++ template-string.js
@@ -0,0 +1,12 @@
+const nome = "Ju";
+const idade = 2026 - 1991;
+const cidadeNascimento = "São paulo";
+/* const apresentacao =
+  "meu nome é " +
+  nome +
+  ", minha idade é " +
+  idade +
+  " e nasci na cidade de " +
+  cidadeNascimento; */
+const apresentacao = `meu nome é ${nome}}, minha idade é ${idade} e nasci na cidade de ${cidadeNascimento}`;
+console.log(apresentacao);
